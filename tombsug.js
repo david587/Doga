2a3faa6f0ebd888e324f17b6c2d858f821279202
Baráth Dávid <davidbarath08@gmail.com>
2022-02-28 09:32:07 +0100

    vége

--- tombsug.js
+++ tombsug.js
@@ -7,6 +7,7 @@
 // Github: https://github.com/david587/
 // Licenc: GNU GPL
 //
+console.log("Baráth Dávid,Szoft1/N,2022.02.28");
 var anyagIn= document.getElementById("anyag");
 var anyagInn= document.getElementById("anyag1");
 var szamitgomb= document.getElementById("szamitgomb");
@@ -17,6 +18,6 @@ szamitgomb.addEventListener("click", function(){
     let anyagg=Number(anyagInn.value);
     var absz = (Math.PI/180)*(anyagg);
     let reszecske = (1/2)*anyag*Math.sin(absz);
-    console.log(reszecske);
-    eredmenyIn.value=reszecske;
+    console.log(reszecske.toFixed(2));
+    eredmenyIn.value=reszecske.toFixed(2);
 });
